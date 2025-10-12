40725e8c1680baa823010d5676c5f5215def8c71
anegre <negre.anapaula@gmail.com>
2025-10-12 12:51:46 -0300

    Better lsb1 test

--- src/main.c
+++ src/main.c
@@ -8,39 +8,16 @@
 int main(int argc, char *argv[]){
     printf("Welcome to stego\n");
 
-    printf("-----------------------Encryption test:-------------------------------------------\n");
+    printf("===============================Encryption test:===========================================\n");
     encryption_test();
     printf("Encryption test finished\n");
 
 
-    printf("-----------------------LSB1 test:-------------------------------------------\n");
+    printf("===============================LSB1 test:=================================================\n");
 
-    const char *input_path  = "public/pepper.bmp";
-    const char *output_path = "public/pepper_stego.bmp";
-    const char *message = "Hello, this is a hidden BMP world!";
-    size_t msg_len = strlen(message) + 1; //  null terminator
+    char * input = "public/pepper.bmp";
+    char * output = "public/pepper_stego.bmp";
+    lsb1_test(input,output);
+    printf("LSB1 test finished\n");
 
-    printf("Loading %s...\n", input_path);
-    struct bmp_image_t *bmp = load_bmp_new(input_path);
-    if (!bmp) return 1;
-
-    printf("Embedding message: \"%s\"\n", message);
-    stego_lsb1(bmp, (const uint8_t *)message, msg_len);
-
-    printf("Saving to %s...\n", output_path);
-    bmp_write(bmp, output_path);
-
-    printf("Reloading %s to extract message\n", output_path);
-    struct bmp_image_t *bmp2 = load_bmp_new(output_path);
-    if (!bmp2) return 1;
-
-    uint8_t *buffer = calloc(1, msg_len);
-    lsb1_extract(bmp2, buffer, msg_len);
-
-    printf("Extracted message: \"%s\"\n", buffer);
-
-    bmp_image_free(bmp);
-    bmp_image_free(bmp2);
-    free(buffer);
-    return 0;
 }
--- src/test/test.h
+++ src/test/test.h
@@ -1,6 +1,11 @@
 #ifndef _TEST_H_
 #define _TEST_H_
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int encryption_test(void);
+void lsb1_test(const char * original_path, const char * output_path);
 
 #endif
